71c82d6992c9b46544f056d432ac557cd5d465da
FRANCINEIDETS <francineide_silva@hotmail.com>
2021-11-15 16:44:50 -0300

    exercicio-variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,92 @@
+// Exercicio
+
+// Exercícios de interpretação de código
+
+// 1
+
+/*let a = 10
+let b = 10
+
+console.log(b) // resposta: console.log(10)
+
+b = 5 
+console.log (a,b) // resposta: console.log(10,5) */
+
+
+// 2
+
+/*let a = 10
+let b = 20
+
+c = a 
+b = c 
+a = c 
+console.log( a, b, c) // resposta: cosnsole.log( 10, 10, 10) */
+
+// 3
+
+/* p = horasPorDia
+t = valorDiario
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+
+alert ('voce recebe: ${t/p} por horas') */ 
+
+// Exercícios de escrita de código
+
+//1
+
+/*let nome ;
+let idade ;
+console.log(typeof nome)
+console.log(typeof idade)
+// a presenta underfined porque não foi declarado valores*/
+
+/*let name = prompt (" Qual o seu nome?")
+let idade = Number(prompt("Qual sua idade?"))
+console.log("Olá", name, "você tem", idade, "anos") */
+
+//2
+
+/*const primeiraPergunta = prompt("Você está usando azul?")
+const segundaPergunta = prompt( "Você gosta de fruta?")
+const terceiraPergunta = prompt("Você é mulher?")
+
+const resposta1 = (primeiraPergunta)
+const resposta2 =  (segundaPergunta)
+const resposta3 = ( terceiraPergunta)
+
+console.log("Você está usando azul?", resposta1)
+console.log("Você gosta de fruta?", resposta2)
+console.log("Você é mulher?", resposta3) */ 
+
+// 3 
+
+/* let a = 10
+let b = 25
+let c
+
+c = a
+a = b 
+b = c
+
+
+//a = 25
+//b = 10
+
+console.log("O novo valor de a é", a)
+console.log( "O novo valor de b é", b) */
+
+// Desafio
+
+/*let numero01 = Number(prompt("insira o primeiro numero:"))
+let numero02 = Number (prompt("Insira o segundo numero:"))
+
+let resposta01 = (numero01 + numero02)
+let resposta02 = (numero01 * numero02)
+
+console.log("A resposta 1 é", resposta01)
+console.log( "A resposta 2 é ", resposta02) */ 
+
+
