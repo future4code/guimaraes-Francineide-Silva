5e7ad3e2a503f7aad344263e8340002962ce8aa5
FRANCINEIDETS <francineide_silva@hotmail.com>
2022-03-10 13:58:54 -0300

    alteracoes-api

--- modulo3/apis-labenusers/apis-labenusers/src/App.js
+++ modulo3/apis-labenusers/apis-labenusers/src/App.js
@@ -1,56 +1,52 @@
 import React from 'react';
-import axios from 'axios';
 import Cadastro from './components/cadastro';
 import Usuario from './components/usuario';
-
-export default class App extends React.Component {
-
-  
-
-  componentDidMount() {
-    this.getAllUsers();
-  };
-
-  
- 
-  render() {
-
-    const cadastroComponents = this.state.usuarios.map((usuario) => {
-      
-      return <li key={usuario.id}> {usuario.name} {usuario.email}</li>;
-    });
-
-    return (
-
-      <Router>
-      <div className="App">
-        <link to ={'/cadastro'}>Cadastro</link>
-        <link to={'/usuario'}>Usuario</link>
-        <Route path = '/cadastro' render ={()=> {
-          <Cadastro
-          onChangeInputUsuario = { this.onChangeInputUsuario.bind(this)}
-          onChangeInputEmail = {this.onChangeInputEmail.bind(this)}
-          createCadastro ={this.createCadastro.bind(this)}
-          />
-
-        }}
-        />
-        <Route path= '/Usuario' render ={()=>(
-        <Usuario
-        cadastroComponent = {cadastroComponents}
-        />
-        
-        )} />
-        
+import Styled from 'styled-components';
 
 
-        
-      </div>
-      </Router >
-    );
+const cardPrincipal =Styled.div`
 
+
+
+`
+
+
+ class App extends React.Component {
+
+
+  state = {
+    tela: "cadastro"
   }
-  
+
+  selecioneTela = () => {
+    switch(this.state.tela){
+       case "cadastro":
+         return <Cadastro irParaLista={this.irParaLista} />
+        case "lista":
+          return <Usuario irParaCadastro={this.irParaCadastro}/>
+        default:
+          return <p>ERROR -TENTE NOVAMENTE</p>
+    }
+  }
+
+  irParaCadastro = () => {
+    this.setState({tela: "cadastro"})
+  }
+
+  irParaLista = () => {
+    this.setState({tela: "lista"})
+  }
+
+
+  render(){
+  return (
+    <div >
+      {this.selecioneTela()}
+    </div>
+  );
+  }
 }
+
+export default App;
 
 
--- modulo3/apis-labenusers/apis-labenusers/src/components/cadastro.jsx
+++ modulo3/apis-labenusers/apis-labenusers/src/components/cadastro.jsx
@@ -1,70 +1,67 @@
 import React from "react";
 import axios from "axios";
-import Usuario from "./usuario";
 
-class Cadastro extends React.Component {
+
+
+ class Cadastro extends React.Component {
     state = {
-        usuarios: []
-    }
-    deletarUsuario = (usuarioId) => {
-        axios.delete("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/",$,{usuarioId}, {
+      nome: "",
+      email: "",
+    };
+  
+    novoNome = (event) => {
+      this.setState({ nome: event.target.value });
+    };
+  
+    novoEmail = (event) => {
+      this.setState({ email: event.target.value });
+    };
+  
+    criaUsuario = async () => {
+      try {
+        const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+        const body = {
+            name: this.state.nome,
+            email: this.state.email
+        }
+        const headers = {
             headers: {
                 Authorization: "francineide-silva-guimaraes"
             }
-        })
-            .then(response => {
-                console.log(response.data)
-                alert("Usuário deletado com sucesso")
-                this.getUserByld()
-            })
-            .catch(error => {
-                console.log(error.message)
-                alert ("Erro ao deletar usuário")
-            })
-    }
-
-    componentDidMount() {
-        axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", {
-            headers: {
-                Authorization: "francineide-silva-guimaraes"
-            }
-        })
-            .then(response => {
-                this.setState({
-                    usuarios: response.data
-                })
-            })
-            .catch(error => {
-                alert ("Erro ao buscar usuários")
-            })
-    }
-
-mostrarUsuarios = () => {
-        axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users', {
-            headers: {
-                Authorization: "francineide-silva-guimaraes"
-            }
-        })
-        const usuarios = this.state.usuarios.map(usuario => {
-            return (
-                <li key={usuario.id}>
-                    {usuario.name}
-                    {usuario.email}
-                    <button onClick={() => this.deletarUsuario(usuario.id)}>Deletar</button>
-                </li>
-            )
-        })
-        return usuarios
-    }
+        }
+    
+        const response = await axios.post(url, body,headers)
+          alert("usuario criado com sucesso")
+          
+      } catch(error) {
+        console.log(error.response.data.message)
+      }
+  
+    };
+  
     render() {
-        return (
-            <div className="App">
-                <h1>Cadastro de Usuários</h1>
-                <ul>
-                    {this.mostrarUsuarios()}
-                </ul>
-            </div>
-        );
+      return (
+        <div>
+          
+          <p>Adicionar Usuario</p>
+          <button onClick={this.props.irParaLista}>Ir para Lista</button>
+          <br/>
+          <br/>
+          <input
+            placeholder="nome"
+            value={this.state.nome}
+            onChange={this.novoNome}
+          />
+          <input
+            placeholder="email"
+            value={this.state.email}
+            onChange={this.novoEmail}
+          />
+          <button onClick={this.criaUsuario}>Cadatro usuário</button>
+
+         
+        </div>
+      );
     }
-}
-export default Cadastro;
+  }
+  export default Cadastro;
