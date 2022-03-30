7d9080564a812ce6332ab5fcab836da83ca6d52b
FRANCINEIDETS <francineide_silva@hotmail.com>
2022-03-29 22:23:26 -0300

    labex-v1

---
+++ modulo4/projeto-labex/labex/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>LABEX</h1>
+      <button>Ver Viagens</button>
+      <button>Area de Admin</button>
+    </div>
+  );
+}
+
+export default App;
