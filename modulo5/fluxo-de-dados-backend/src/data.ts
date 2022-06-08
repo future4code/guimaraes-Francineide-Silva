f16aa94a0303e1b26fb518b8748a6728a299c9a8
FRANCINEIDETS <francineide_silva@hotmail.com>
2022-06-07 22:49:40 -0300

    Exercicio-fluxo-de-dados

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,48 @@
+
+type UserProduct ={
+
+    id: number;
+    name: string;
+    price: number
+
+}
+
+export const products: UserProduct[] =[
+
+    {
+        id: 1,
+        name:"serenata de amor",
+        price: 20
+
+    },
+    {
+        id: 2,
+        name: "doce de Leite",
+        price:15
+    },
+
+    {
+        id: 3,
+        name: "doce de banana",
+        price: 18
+
+
+    },
+    {
+        id: 4,
+        name:"pipoca",
+        price:5
+    },
+    {
+        id: 5,
+        name: "feijão",
+        price: 12
+    },
+    {
+        id: 6,
+        name:"arroz",
+        price: 8
+    }
+
+
+]
